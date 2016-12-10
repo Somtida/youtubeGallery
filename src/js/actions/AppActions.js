30560b6ef7efb1723c817caf0776c4a3c5f94649
S.SKNS <somtida.th@gmail.com>
2016-12-09 23:44:48 -0800

    addForm

--- src/js/actions/AppActions.js
+++ src/js/actions/AppActions.js
@@ -2,7 +2,10 @@ var AppDispatcher = require('../dispatcher/AppDispatcher');
 var AppConstants = require('../constants/AppConstants');
 
 var AppActions = {
-  
+  saveVideo(video) {
+  	console.log(video);
+  }
+
 
 }
 
