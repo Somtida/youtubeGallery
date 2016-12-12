140aeb047aa768ac7c9d9a54497aab51bd2cf30f
S.SKNS <somtida.th@gmail.com>
2016-12-12 00:11:23 -0800

    receiveVideo

--- src/js/utils/appAPI.js
+++ src/js/utils/appAPI.js
@@ -10,8 +10,16 @@ module.exports = {
   getVideos() {
     this.firebaseRef = new Firebase("https://mygallery-3b400.firebaseio.com/videos");
     this.firebaseRef.once("value", (snapshot) => {
+      var videos = [];
       snapshot.forEach(ele => {
-        console.log(ele.val());
+        var video = {
+          id: ele.key(),
+          title: ele.val().title,
+          video_id: ele.val().video_id,
+          description: ele.val().description,
+        }
+        videos.push(video);
+        AppActions.receiveVideos(videos);
       })
     });
   },
