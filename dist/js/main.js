c7f750c18b9ce5e93a8e54abb975e01f2c5ff146
S.SKNS <somtida.th@gmail.com>
2016-12-11 23:57:33 -0800

    saveInFirebase

--- dist/js/main.js
+++ dist/js/main.js
@@ -20288,7 +20288,9 @@ var AppActions = require('../actions/AppActions');
 
 module.exports = {
   saveVideo(video) {
-    console.log('API called...')
+    console.log('API called...');
+    this.firebaseRef = new Firebase("https://mygallery-3b400.firebaseio.com/videos");
+    this.firebaseRef.push(video);
   },
 }
 
