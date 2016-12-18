2b65dbe7bf154fdff7ffb8c11a4b466c72ef2708
S.SKNS <somtida.th@gmail.com>
2016-12-17 21:22:24 -0800

    Update Video.js

--- src/js/components/Video.js
+++ src/js/components/Video.js
@@ -3,6 +3,7 @@ var AppActions = require('../actions/AppActions');
 var AppStore = require('../stores/AppStore');
 
 var Video = React.createClass({
+  var link = <iframe width="560" height="315" src="https://www.youtube.com/watch?v=bFmThPwKRGU&sns=em" frameborder="0" allowfullscreen></iframe>
   render(){
     return(
       <div>
