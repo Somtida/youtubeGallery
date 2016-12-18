1f4f00e14f496583019bcce1af189376b9588130
S.SKNS <somtida.th@gmail.com>
2016-12-17 21:28:34 -0800

    Update Video.js

--- src/js/components/Video.js
+++ src/js/components/Video.js
@@ -3,12 +3,14 @@ var AppActions = require('../actions/AppActions');
 var AppStore = require('../stores/AppStore');
 
 var Video = React.createClass({
-  var link = "";
+  var link = "https://www.youtube.com/embed/"+this.props.video.video_id;;
   render(){
     return(
       <div className="c4">
       <label>title</label>
       <h5>{this.props.video.title}</h5>
+      <iframe width="560" height="315" src={link} frameborder="0" allowfullscreen></iframe>
+  
       </div>
     )
   }
